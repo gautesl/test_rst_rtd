dafb1be460dda591cee1901c58db8a46cffb35be
Gaute Svanes Lunde <gaute.lunde@nordicsemi.no>
2022-06-13 13:06:08 +0200

    theme

--- doc/conf.py
+++ doc/conf.py
@@ -48,15 +48,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_logo = "_static/images/logo.png"
+html_favicon = "_static/images/favicon.ico"
+html_static_path = ["_static"]
+html_theme_options = {
+    "logo_only": True,
+    "github_url": "https://github.com/gautesl/test_rst_rtd",
+    "repository_url": "https://github.com/gautesl/test_rst_rtd",
+    "use_edit_page_button": True,
+    "repository_branch": "master",
+    "path_to_docs": "doc",
+}
 
 # -- Options for zephyr.external_content ----------------------------------
 
